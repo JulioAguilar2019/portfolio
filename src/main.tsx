28623133e825c5aa3424f839aacfa02c50b8e21e
Julio Aguilar <julioa.m777@hotmail.com>
2023-03-02 15:15:56 -0600

    [ADD] i18next and fix some bugs of frontPage

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,30 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import { I18nextProvider } from 'react-i18next';
+import i18next from 'i18next';
+import global_es from './i18n/es/global.json';
+import global_en from './i18n/en/global.json';
 
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: 'en',
+  resources: {
+    es: {
+      global: global_es,
+    },
+    en: {
+      global: global_en,
+    },
+  }
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter >
-      <App />
-    </BrowserRouter>
+    <I18nextProvider i18n={i18next}>
+      <BrowserRouter >
+        <App />
+      </BrowserRouter>
+    </I18nextProvider>
   </React.StrictMode>,
 )
--- src/pages/Home/components/Frontpage.tsx
+++ src/pages/Home/components/Frontpage.tsx
@@ -1,19 +1,22 @@
 import picture from '@/assets/home/firstImage.png'
+import { useTranslation } from 'react-i18next'
 
 export const Frontpage = () => {
+
+    const [t] = useTranslation('global')
     return (
 
         <>
             <div className="mt-8 lg:-mx-6 lg:flex lg:items-center">
 
                 <div className="mt-6 lg:w-1/2 lg:mt-0 lg:mx-6 ">
-                    <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-white md:text-5xl lg:text-6xl">I’m a<span className="text-primary"> front-end developer</span> from El Salvador</h1>
+                    <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-white md:text-5xl lg:text-6xl">{t("home.greeting")}<span className='text-primary'> {t("home.tech")}</span> {t("home.country")}</h1>
 
                     <p className="mt-3 text-sm text-secondary">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veritatis sint autem nesciunt,
                         laudantium quia tempore delect
                     </p>
-                    <button type="button" className="py-2.5 px-5 my-4 text-sm font-medium text-white focus:outline-none border border-primary hover:bg-primary100">Download resume</button>
+                    <button type="button" className="py-2.5 px-5 my-4 text-sm font-medium text-white focus:outline-none border border-primary hover:bg-primary100">{t("home.button")}</button>
 
                 </div>
                 <img className="object-scale-down w-full lg:mx-6 lg:w-1/2 rounded-xl h-72 lg:h-96" src={picture} alt="" />
@@ -23,7 +26,7 @@ export const Frontpage = () => {
 
             <div className="py-10 w-3/4 mt-10 mx-auto border border-secondary">
                 <blockquote>
-                    <p className="text-2xl italic font-medium text-secondary text-center overflow-auto">"I’m not a great programmer; I’m just a good programmer with great habits."</p>
+                    <p className="text-2xl italic font-medium text-secondary text-center overflow-auto">"{t("home.quote")}"</p>
                 </blockquote>
                 <p className="pr-3 font-light text-center text-secondary">- Kent Beck</p>
             </div>
